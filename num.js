5687dec7fde7705693ca41c1f9ad62920e415e64
dveinadams <159923051+dveinadamsf1796@users.noreply.github.com>
2024-03-06 08:39:13 +0100

    Create num.js

---
+++ num.js
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2;
+        System.out.println("Сумма чисел " + num1 + " и " + num2 + " равна " + sum);
+    }
+}
