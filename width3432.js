2902bfeea24081a61555d35133fd0ed4e8ce3458
dveinadams <159923051+dveinadamsf1796@users.noreply.github.com>
2024-04-06 05:25:09 +0200

    Create width3432.js

---
+++ width3432.js
@@ -0,0 +1,8 @@
+public class RectangleArea {
+    public static void main(String[] args) {
+        double length = 5.5;
+        double width = 4;
+        double area = length * width;
+        System.out.println("Area of rectangle: " + area);
+    }
+}
