2350ed266b419ade73d5d9881501d3f4a04653aa
dveinadams <159923051+dveinadamsf1796@users.noreply.github.com>
2024-04-26 03:34:29 +0200

    Create argsdfvbdfvbfd.js

---
+++ argsdfvbdfvbfd.js
@@ -0,0 +1,17 @@
+public class PrimeNumberCheck {
+    public static void main(String[] args) {
+        int number = 29;
+        boolean isPrime = true;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
