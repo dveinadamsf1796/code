76cc41dbf01a70bb5be78e6e87000f8719d4d4a1
dveinadams <159923051+dveinadamsf1796@users.noreply.github.com>
2024-05-06 04:49:22 +0200

    Create Stringfdvwfedf.js

---
+++ Stringfdvwfedf.js
@@ -0,0 +1,10 @@
+public class EvenOddCheck {
+    public static void main(String[] args) {
+        int num = 6;
+        if (num % 2 == 0) {
+            System.out.println(num + " is even.");
+        } else {
+            System.out.println(num + " is odd.");
+        }
+    }
+}
